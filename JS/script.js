288ffd3c36e340b7532ea8ab84c8f78c7923f8b8
PrathamMittal-Tech <Prathamv0106@gmail.com>
2024-11-06 20:05:02 +0530

    Update JS

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,6 @@
 let username = document.querySelector('.username');
 let password = document.querySelector('.password');
 let btn = document.querySelector('.btn');
-let btnLink = document.querySelector('.login-link');
-let login = document.querySelector('.navbar-login');
-let loginBtn = document.querySelector('.login-btn');
-let listGroup = document.querySelector('.list-group');
 
 let myusername = "Pratham123";
 let mypassword = "123456";
@@ -24,14 +20,3 @@ btn.onclick = (event) => {
         alert("Invalid username or password!");
     }
 };
-
-loginBtn.onclick = () => {
-    if (num==0){
-        listGroup.style.display = "list-item";
-        num=1;
-    }
-    else{
-        listGroup.style.display = "none";
-        num=0;
-    }
-}
