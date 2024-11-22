705cd394562e06de29e0242228078a69f7e1e7b6
PrathamMittal-Tech <Prathamv0106@gmail.com>
2024-11-22 19:02:23 +0530

    ChatBot Integrated

---
+++ JS/login.js
@@ -0,0 +1,24 @@
+let username = document.querySelector('.username');
+let password = document.querySelector('.password');
+let btn = document.querySelector('.btn');
+let loginPage = document.querySelector('.navbar-login');
+
+let myusername = "cookie rookie";
+let mypassword = "123456";
+
+
+btn.onclick = (event) => {
+    event.preventDefault();
+    if (username.value==="" && password.value===""){
+        console.log("No Field entered")
+    }
+    else if (username.value===myusername && password.value===mypassword){
+        loginPage.style.display = "none";
+
+    }
+    else {
+        alert("Invalid username or password!");
+    }
+};
+
+
